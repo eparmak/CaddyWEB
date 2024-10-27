d5fe9a7d1e0cb95061ab842e565443eb2bca20cb
root <root@caddyweb.192.168.130.22>
2024-10-27 09:03:32 +0000

    Removed Files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$confdir = '/etc/caddy/caddy.conf.d/';
-	$certsdir = '/etc/caddy/certs/';
-	$failover_maxfails = 3;
-	$failover_failduration = '30s';
-	$letsencryptmail = 'maidawmn@gmail.com';
-	$basecaddyconfig = '/etc/caddy/Caddyfile';
-	
-?>
