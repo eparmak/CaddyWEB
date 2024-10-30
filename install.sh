931cd36c715eaff71454813f0e7bcf60577ad4b9
root <root@caddyweb.192.168.130.22>
2024-10-30 21:17:49 +0000

    Updated Install Script

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 bash scripts/installreq.sh
-cp -R etc/caddy /etc/caddy
-cp -R usr/share/ /usr/share
+cp -R etc/caddy/* /etc/caddy
+cp -R usr/share/* /usr/share
 service caddy restart
+
 caddy reload --config /etc/caddy/Caddyfile
 echo "Caddy Web Installed..."
